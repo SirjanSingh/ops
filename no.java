9022810e59dda9930d178f60a29767453b9e7d60
Sirjan Singh <sirjan.singh036@gmail.com>
2024-10-15 00:35:42 +0530

    yayyy

---
+++ no.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class no {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner (System.in); String s
+    ;
+    s = sc . nextLine (); System.out . println (s + " is a palindrome")
+
+;
+sc.close()    ;}
+}
